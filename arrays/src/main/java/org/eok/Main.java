1385d3f50effd930d4f1e208024ee25465d4cb7e
Edward Okello <eokello@INDRALIMITED.local>
2024-10-07 14:38:36 +0300

    arrays

--- arrays/src/main/java/org/eok/Main.java
+++ arrays/src/main/java/org/eok/Main.java
@@ -7,9 +7,11 @@ public class Main {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
 
+        System.out.println("This is arrays:");
         // Store roll numbers
         int[] rollNo = new int[5]; // declare and initialize
         for (int i = 0; i < rollNo.length; i++) {
+            System.out.println("Enter roll number: "+ i);
             rollNo[i] = in.nextInt();
         }
         // printing array elements
