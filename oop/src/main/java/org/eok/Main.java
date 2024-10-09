a43e4deee44cb27681aa6c5de724de75ec3c1159
Edward Okello <eokello@INDRALIMITED.local>
2024-10-09 23:12:35 +0300

    constructors

--- oop/src/main/java/org/eok/Main.java
+++ oop/src/main/java/org/eok/Main.java
@@ -6,14 +6,18 @@ public class Main {
         System.out.println("Store student information:");
 
         Student john = new Student();
-        john.marks = 90.8f;
-        john.name = "John Akhudu";
-        john.rollNo = 1328;
+        Student ole = new Student(200, "Ole Masaai", 98.9f);
+        Student random = new Student(john);
+
+        john.changeName("Akudu");
+        john.greeting();
 
         System.out.println(john);
         System.out.println(john.rollNo);
         System.out.println(john.name);
         System.out.println(john.marks);
+
+        System.out.println(random.name);
     }
 }
 
@@ -21,4 +25,30 @@ class Student {
     int rollNo;
     String name;
     float marks = 99;
+
+    // add the values of the properties to the constructor
+    Student () {
+        this.rollNo = 20;
+        this.name = "Student name";
+        this.marks = 79.9f;
+    }
+
+    Student (int rollNo, String name, float marks){
+        this.rollNo = rollNo;
+        this.name = name;
+        this.marks = marks;
+    }
+
+    Student (Student other) {
+        this.name = other.name;
+        this.rollNo = other.rollNo;
+        this.marks = other.marks;
+    }
+    void greeting(){
+        System.out.println("Hello there, my name is: " + this.name);
+    }
+
+    void changeName(String newName) {
+        this.name = newName;
+    }
 }
