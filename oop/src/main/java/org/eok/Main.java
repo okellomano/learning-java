f12f5b77cb393ca69993794e7fa738a1f571c9aa
Edward Okello <eokello@INDRALIMITED.local>
2024-10-09 22:11:04 +0300

    oop

--- oop/src/main/java/org/eok/Main.java
+++ oop/src/main/java/org/eok/Main.java
@@ -3,6 +3,22 @@ package org.eok;
 public class Main {
     public static void main(String[] args) {
 
-        System.out.println("Hello world!");
+        System.out.println("Store student information:");
+
+        Student john = new Student();
+        john.marks = 90.8f;
+        john.name = "John Akhudu";
+        john.rollNo = 1328;
+
+        System.out.println(john);
+        System.out.println(john.rollNo);
+        System.out.println(john.name);
+        System.out.println(john.marks);
     }
+}
+
+class Student {
+    int rollNo;
+    String name;
+    float marks = 99;
 }
